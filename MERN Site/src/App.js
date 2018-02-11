1734fc93f14abece32d0c328378b69d3e6113c89
Ryan Hanchett <hanchett_ryan@yahoo.com>
2018-02-11 15:01:33 -0800

    Styled and added content to home page to mirror mock-ups

--- MERN Site/src/App.js
+++ MERN Site/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import './App.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import './App.css';
+
 
 class App extends Component {
 
@@ -24,16 +26,41 @@ class App extends Component {
 
   render() {
     return (
-      <div class="container" class="text-right">
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <ul>
-              <li>About</li>
-              <li>Services</li>
-              <li>Login</li>
-              <li><span class="glyphicon glyphicon-user" aria-hidden="true"></span></li>
-            </ul>
-        </nav>
+      <div>
+        <div class='navigation'>
+          <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="#">METS</a>
+              </div>
+              <ul class="nav navbar-nav navbar-right">
+                <li>About</li>
+                <li>Services</li>
+                <li>Login</li>
+                <li><i class="far fa-user"></i></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div class='body'>
+          <div class='search'>
+            <h2>Find the best technologies for your students with us</h2>
+            <form>
+              <div class="form-group">
+                <input type="text" class="form-control" id="subject" aria-describedby="subject" placeholder="Subject" />
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="learningBarrier" placeholder="Learning Barrier" />
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="Learning Style" placeholder="Learning Style" />
+              </div>
+              <div class='advSearch'>Advanced Search</div> 
+              <button type="submit" class="btn btn-primary">Search</button>
+            </form>
+          </div>
 
+        </div>
       </div>
     );
   }
