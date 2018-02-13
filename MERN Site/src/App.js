80b8c31db036f5845bf2724ad9c7dd740c109ed5
Ryan Hanchett <hanchett_ryan@yahoo.com>
2018-02-12 19:25:02 -0800

    Added carousel icons

--- MERN Site/src/App.js
+++ MERN Site/src/App.js
@@ -26,14 +26,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
-        <div class='navigation'>
-          <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-              <div class="navbar-header">
-                <a class="navbar-brand" href="#">METS</a>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+        <div className='navigation'>
+          <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <a className="navbar-brand" href="#">METS</a>
               </div>
-              <ul class="nav navbar-nav navbar-right">
+              <ul className="nav navbar-nav navbar-right">
                 <li>About</li>
                 <li>Services</li>
                 <li>Login</li>
@@ -42,23 +42,47 @@ class App extends Component {
             </div>
           </nav>
         </div>
-        <div class='body'>
-          <div class='search'>
-            <h2>Find the best technologies for your students with us</h2>
-            <form>
-              <div class="form-group">
-                <input type="text" class="form-control" id="subject" aria-describedby="subject" placeholder="Subject" />
+        <div className='body'>
+
+          <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <div className='search'>
+                  <h2>Find the best technologies for your students with us</h2>
+                  <form>
+                    <div className="form-group">
+                      <input type="text" className="form-control" id="subject" aria-describedby="subject" placeholder="Subject" />
+                    </div>
+                    <div className="form-group">
+                      <input type="text" className="form-control" id="learningBarrier" placeholder="Learning Barrier" />
+                    </div>
+                    <div className="form-group">
+                      <input type="text" className="form-control" id="Learning Style" placeholder="Learning Style" />
+                    </div>
+                    <div className='advSearch'>Advanced Search</div>
+                    <button type="submit" className="btn btn-primary">Search</button>
+                  </form>
+                </div>
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" id="learningBarrier" placeholder="Learning Barrier" />
+
+              <div className="carousel-item">
+                Blah
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" id="Learning Style" placeholder="Learning Style" />
+              <div className="carousel-item">
+                blah 2
               </div>
-              <div class='advSearch'>Advanced Search</div> 
-              <button type="submit" class="btn btn-primary">Search</button>
-            </form>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
+            </a>
           </div>
+
+
 
         </div>
       </div>
