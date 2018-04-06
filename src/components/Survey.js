6bf3e5c9f93b784ec8a1af3b383b981b6dc78e55
Ryan Hanchett <hanchett_ryan@yahoo.com>
2018-04-06 14:10:23 -0700

    Changes to survey

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import './css/Survey.css';
 
@@ -8,31 +9,22 @@ class Survey extends Component {
         super(props);
         this.state = {
             question: 0,
-            q1: '',
-            q2: '',
-            q3: '',
-            q4: '',
-            q5: '',
-            q6: '',
-            q7: '',
-            q8: ''
+            answers: []
         }
     }
 
-    finalQuestion(e) {
-        let question = this.state.question;
-        console.log(e.target.id);
-        this.setState({
-            question: e.target.id
-        });
-    }
     answeredQuestion(e) {
         let question = this.state.question;
         console.log(e.target.id);
         this.setState({
             question: e.target.id
         });
-        this.nextQ(e);
+
+        if(e.target.parentNode.id == 'last') {
+            console.log(this);
+        } else {
+            this.nextQ(e);
+        }
     }
 
     // Moves current question off the screen and brings up the new question 
@@ -40,7 +32,11 @@ class Survey extends Component {
         e.target.parentNode.style.left = '0';
         e.target.parentNode.style.transform = 'translate(-100%)';
         let newQuestion = 'q' + (this.state.question + 1);
-
+        let newAnswers = this.state.answers; 
+        newAnswers.push(e.target.id);
+        this.setState({
+            answers: newAnswers
+        });
         // Using set state callback to ensure it is up to date when we call the new question
         this.setState({ question: (this.state.question + 1) }, function () {
             let newElement = this.refs[newQuestion];
@@ -129,7 +125,7 @@ class Survey extends Component {
                     </button>
                 </div>
                 <div className="question questionSix row" ref='q6'>
-                    <h1>Do you work with students with disabilities? Please select all that apply</h1>
+                    <h1 className='col-lg-12'>Do you work with students with disabilities? Please select all that apply</h1>
                     <button id='1' className="qBtn col-md-4" onClick={this.answeredQuestion.bind(this)}>
                         Auditory perception & processing
                     </button>
@@ -163,20 +159,17 @@ class Survey extends Component {
                         No
                     </button>
                 </div>
-                <div className="question questionSeven row" ref='q8'>
+                <div className="question questionEight row" id='last' ref='q8'>
                     <h1>
                         Do you work with students who miss class more than 25 percent of the time?
                     </h1>
-                    <Link to='Search'>
-                        <button className='qBtn' id='1' onClick={this.finalQuestion.bind(this)}>
-                            Yes
-                        </button>
-                    </Link>
-                    <Link to='Search'>
-                        <button className='qBtn' id='2' onClick={this.finalQuestion.bind(this)}>
-                            No
-                        </button>
-                    </Link>
+                    <button className='qBtn' id='1' onClick={this.answeredQuestion.bind(this)}>
+                        Yes
+                    </button>
+                    <button className='qBtn' id='2' onClick={this.answeredQuestion.bind(this)}>
+                        No
+                    </button>
+
                 </div>
             </div>
         );
