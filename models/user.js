ba704957a52ecd7dca4ec2c9046f4f24c735a5f5
Shigenaka <mshig19@uw.edu>
2018-04-29 15:30:02 -0700

    got login working, set up for logging user logins

--- models/user.js
+++ models/user.js
@@ -12,12 +12,12 @@ var UserSchema = new mongoose.Schema({
 
 });
 
-// UserSchema.methods.generateHash = function(password) {
-//     return bcrypt.hash(password, bcrypt.genSaltSync(8), null);
-// };
+UserSchema.methods.generateHash = function(password) {
+     return bcrypt.hash(password, bcrypt.genSaltSync(8), null);
+};
 
-// UserSchema.methods.validPassword = function(password) {
-//     return bcrypt.compareSync(password, this.password);
-// }
+UserSchema.methods.validPassword = function(password) {
+     return bcrypt.compareSync(password, this.password);
+}
 
 module.exports = mongoose.model("User", UserSchema);
