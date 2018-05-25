1b75c700269207110741e1a4f1b7196f39baf090
Ryan Hanchett <hanchett_ryan@yahoo.com>
2018-05-25 11:52:31 -0700

    Added match page

--- src/components/SurveyResults.js
+++ src/components/SurveyResults.js
@@ -19,7 +19,7 @@ class SurveyResults extends Component {
     loadCards() {
         axios.get(`http://localhost:3101/search`).then(res => {
             let prodData = res.data;
-            prodData = prodData.slice(15, 27);
+            prodData = prodData.slice(15, 21);
             this.setState({
                 prods: prodData
             });    
@@ -43,12 +43,19 @@ class SurveyResults extends Component {
     render() {
         console.log(this.state);
         let paddingTop = this.state.displaySideNav === true ? '130px' : '0px';
+
         return (
             <div>
                 <NavBar />
                 <SideNav callback={this.addMargin.bind(this)} />
-                <SurveyResultCards filters={this.state.filters}/>
-
+                <div className="results" style={{ paddingTop: paddingTop }}>
+                    <div className='row'>
+                        {this.state.prods.map(card => {
+                            return <SurveyResultCards key={card._id} id={card._id} title={card.title} image={card.image} summary={card.summary} rating={card.rating} tags={card.tags}/>;
+                        })}
+                    </div>
+                </div>
+              
 
 
             </div>
